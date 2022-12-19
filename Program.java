aa5d5b0f9790a4424828464f68cc3d01e1421980
Vyacheslav <vmk_86@mail.ru>
2022-12-19 17:18:31 +0300

    save8

---
+++ Program.java
@@ -0,0 +1,14 @@
+public class Program {
+    public static void main(String[] args) {
+        
+        Zoo zoo = new Zoo();
+        System.out.println("speak");
+        zoo.talk();
+        System.out.println("run");
+        zoo.run();
+        System.out.println("fly");
+        zoo.fly();
+        System.out.println("swim");
+        zoo.swim();
+    }
+}
