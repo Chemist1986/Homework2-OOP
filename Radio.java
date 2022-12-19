98b6b8352a8b76aa2b08e57fb26ce29faed77847
Vyacheslav <vmk_86@mail.ru>
2022-12-19 17:21:23 +0300

    save9

---
+++ Radio.java
@@ -0,0 +1,11 @@
+public class Radio implements Speakable{
+    
+    public String speak() {
+        return "bla bla bla";
+    }
+
+    @Override
+    public String toString() {
+        return "Radio";
+    } 
+}
