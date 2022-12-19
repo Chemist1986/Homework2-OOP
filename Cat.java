c4bb75981e0e63e0c764a49bba82fd361fe2d9c0
Vyacheslav <vmk_86@mail.ru>
2022-12-19 16:56:01 +0300

    save2

---
+++ Cat.java
@@ -0,0 +1,16 @@
+public class Cat extends Animal implements Speakable, Runable{
+
+    public Cat(String name, String breed, String colour, Integer countLegs, Integer age) {
+        super(name, breed, colour, countLegs, age);
+    }
+
+    @Override
+    public String speak() {
+        return "Mau...";
+    }
+
+    @Override
+    public String run() {
+        return "50 km/h";
+    }
+}
